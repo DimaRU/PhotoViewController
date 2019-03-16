a2ece42c16363248eb43755065e97996f927e5a2
Dmitriy Borovikov <dmitry@borovikov.name>
2019-03-16 16:53:51 +0300

    Initial Commit

---
+++ PhotoViewController/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  PhotoViewController
+//
+//  Created by Dmitriy Borovikov on 16/03/2019.
+//  Copyright © 2019 Dmitriy Borovikov. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+}
---
+++ PhotoViewController/PhotoViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  PhotoViewController
+//
+//  Created by Dmitriy Borovikov on 15/03/2019.
+//  Copyright © 2019 Dmitriy Borovikov. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var photos: [URL] = [
+        URL(string: "https://static.inaturalist.org/photos/99279/medium.jpg?1545393634")!,
+        URL(string: "https://static.inaturalist.org/photos/17415659/medium.jpg?1525460504")!,
+        URL(string: "https://static.inaturalist.org/photos/17415663/medium.jpg?1525460506")!,
+        URL(string: "https://static.inaturalist.org/photos/17415666/medium.jpg?1525460508")!,
+        URL(string: "https://static.inaturalist.org/photos/17415667/medium.jpg?1525460510")!,
+    ]
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pageControl.numberOfPages = photos.count
+    }
+    
+    func makeImage(for url: URL) -> UIImage? {
+        guard let photo = try? Data(contentsOf: url) else { return nil }
+        guard let photoImage = UIImage(data: photo) else { return nil }
+        return photoImage
+    }
+    
+    var scollViewIsDragging = false
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        scollViewIsDragging = true
+    }
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        scollViewIsDragging = false
+    }
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard scollViewIsDragging else { return }
+        let visibleRect = CGRect(origin: collectionView.contentOffset, size: collectionView.bounds.size)
+        let visiblePoint = CGPoint(x: visibleRect.midX, y: visibleRect.midY)
+        if let visibleIndexPath = collectionView.indexPathForItem(at: visiblePoint) {
+            pageControl.currentPage = visibleIndexPath.row
+        }
+    }
+    
+    @IBAction func setPage(_ sender: UIPageControl) {
+        let indexPath = IndexPath(item: sender.currentPage, section: 0)
+        collectionView.scrollToItem(at: indexPath, at: [.centeredVertically, .centeredHorizontally], animated: true)
+    }
+}
+
+extension PhotoViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCollectionViewCell", for: indexPath) as! PhotoCollectionViewCell
+        cell.imageView.contentMode = .scaleAspectFit
+        cell.imageView.image = makeImage(for: photos[indexPath.row])
+        return cell
+    }
+}
+
+extension PhotoViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected: \(indexPath.row)")
+    }
+}
+
+extension PhotoViewController: UICollectionViewDataSourcePrefetching {
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        
+    }
+}
+
+extension PhotoViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.frame.size
+    }
+}
